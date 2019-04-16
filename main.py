5cea3f23887ab3e131b8f468fde7a6fac8647db1
MushuYoWushu <daethmon7@gmail.com>
2019-04-16 15:02:01 -0700

    Testing numpy

--- main.py
+++ main.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import numpy as np
+
 print("Hello, my name is Nathan, I like eggs and am learning to read numbers.")
 print("I also like cats.")
 
