14afcf1061c6c761fe2e57eea200198f71a0d674
MushuYoWushu <daethmon7@gmail.com>
2019-04-21 00:22:20 -0700

    Wrote code for image processing and backpropagation.
    Still need to think through logic and get arrays to match with bias units

--- main.py
+++ main.py
@@ -15,8 +15,8 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from csv import reader
-from numpy import random, exp, fromstring
-#  array, dot,
+from numpy import random, exp, fromstring, dot, array, mean, arange, zeros_like
+
 
 print("Hello, my name is Nathan, I like eggs and am learning to read numbers.")
 print("I also like cats and talking about myself.")
@@ -35,12 +35,12 @@ class NateBrain(object):
         print("I am NateBrain, my seed number is ", self.seed)
         print("I am NateBrain, my number of input neurons is ", self.input_num)
         print("I am NateBrain, my number of hidden neurons is ", self.hidden_neurons)
-        print("I am NateBrain, my number of output neurons is ", self.hidden_neurons)
+        print("I am NateBrain, my number of output neurons is ", self.output_neurons)
 
     def __init__(self):
         random.seed(self.seed)  # User can change the seed that a nate is made with
-        self.synapse1 = .1*random.random((self.input_num + 1, self.hidden_neurons)) - .05       # See 'Notes - Deriving the weights'
-        self.synapse2 = .1*random.random((self.hidden_neurons + 1, self.output_neurons)) - .05  # and 'Notes - Array Size' for explanation
+        self.synapse1 = array(.1*random.random((self.input_num + 1, self.hidden_neurons + 1)) - .05)   # See 'Notes'
+        self.synapse2 = array(.1*random.random((self.hidden_neurons + 1, self.output_neurons)) - .05)  # for explanation
 
     def sigmoid(self, x):  # We do not change the class contents so the IDE may suggest making static
         return 1 / (1 + exp(-x))
@@ -48,14 +48,38 @@ class NateBrain(object):
     def sigmoid_deriv(self, x):  # We do not change the class contents so the IDE may suggest making static
         return x * (1-x)    # Note this is equivalent to pow((1 + exp(-x)), 2)
 
+    def label2output(self, num_label):  # This translates an int to an array with a 1 in the correct index
+        output = zeros_like(arange(self.output_neurons))  # makes a 0'd array of output_neurons size
+        output[num_label] = 1  # sets the entry of the corresponding digit to 1
+        return output
+
     def train(self, csv_file_path):  # Trains based on passed data location
         with open(csv_file_path) as imgs:
              for number_img in imgs:  # Here we are going to process each image piece by piece
                 image = fromstring(number_img, dtype=int, sep=',')
-                number_label = image[0]  # This is the label of what the data in number_data represents
-                number_data = image[1:]  # This is the data that represents a handwritten number
+                output = self.label2output(image[0])  # image[0] is the data label, this is an array translation.
+                number_data = image  # bias neuron and image data
+                number_data[0] = 1  # set bias to 1
 
-# NateBrain ends here
+                l0 = number_data  # this series of dots and sigmoids represent the transfer from start to end
+                l1 = self.sigmoid(dot(l0, self.synapse1))  # between neurons, through synapses
+                l2 = self.sigmoid(dot(l1, self.synapse2))
+
+                print("Synapse 1 shape: ", self.synapse1.shape, "Synapse 2 shape: ", self.synapse2.shape)
+                l2_err = output - l2
+                print("l2 err value is ", mean(abs(l2_err)))
+
+                l2_delta = self.sigmoid_deriv(l2) * l2_err  # this tells us the amount weight gotta change on l2
+                l1_err = dot(l2_delta, self.synapse1.T)  # backprop step that shows how much l1 contributed to error
+                l1_delta = self.sigmoid_deriv(l1) * l1_err  # this tells us the amount weight gotta change on l1
+
+                self.synapse2 += dot(l1.T, l2_delta)
+                self.synapse1 += dot(l0.T, l1_delta)
+                break
+
+
+
+             # NateBrain ends here
 
 
 # Testing Suite
@@ -78,9 +102,10 @@ nate.train("mnist_train.csv")
 
 # Array Size
 # A synapse layer needs to be an array of the total inputs from the previous layer (including bias), x
-# crossed by the number of neurons in the next layer, y
+# crossed by the number of neurons in the next layer, y. In this case we need to make space for the bias in two places,
+# on the input layer and on the hidden layer so we add one space to the array
 #
-# Layer 1 = 784 inputs + 1 bias and n hidden layer neurons -> (input_num + 1, hidden_neurons)
+# Layer 1 = 784 inputs + 1 bias and n hidden layer neurons -> (input_num + 1, hidden_neurons + 1)
 # Layer 2 = n hidden layer neurons + 1 bias and the number of outputs -> (hidden_neurons + 1, output_neurons)
 
 # Sigmoid Functions
